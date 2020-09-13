907d0c48830f9b124f86994fb063aca1cd31362f
Winrida <winrida@yandex.ru>
2020-09-14 00:16:52 +0300

    Fix_1

--- src/ru/mirea/pr3/Recursion_1.java
+++ src/ru/mirea/pr3/Recursion_1.java
@@ -2,12 +2,12 @@ package ru.mirea.pr3;
 
 public class Recursion_1
 {
-    public static int recursion(int k)
+    public static String recursion(int k)
     {
         int a = 0, b = 0;
         if(k == 1)
         {
-            return 1;
+            return "1";
         }
         for (int i = 0; a < k; i++)
         {
@@ -16,10 +16,10 @@ public class Recursion_1
         }
 
         System.out.print(recursion(--k) + " " + b);
-        return 0;
+        return "";
     }
 
     public static void main(String[] args) {
-        System.out.print(recursion(5));
+        System.out.print(recursion(7));
     }
 }
