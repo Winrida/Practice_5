8e8d236b70b9713db475ddd7e4cf92a7bc67e44e
Winrida <winrida@yandex.ru>
2020-09-14 22:35:19 +0300

    +Rec_3

--- src/ru/mirea/pr3/Recursion_3.java
+++ src/ru/mirea/pr3/Recursion_3.java
@@ -1,6 +1,25 @@
 package ru.mirea.pr3;
 
-public class Recursion_3
-{
-
+public class Recursion_3 {
+    public static String recursion(int a, int b) {
+        if (a > b)
+        {
+            if (a == b)
+            {
+                return "A = B";
+            }
+            return a + " " + recursion(a - 1, b);
+        } else
+            {
+            if (a == b)
+            {
+                return "A = B";
+            }
+            return a + " " + recursion(a + 1, b);
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println(recursion(10, 2)); // вызов рекурсивной функции
+        System.out.println(recursion(3, 13)); // вызов рекурсивной функции
+    }
 }
